a1139408bc77a180c178dbbb71b3409f659dea9d
CPatchane <code@patchane.com>
2017-10-19 18:27:46 +0200

    fix: better eslint config part + add prettier formatter in app template :pencil2:

--- packages/eslint-config-cozy-app/index.js
+++ packages/eslint-config-cozy-app/index.js
@@ -12,3 +12,18 @@ module.exports = {
     es6: true
   }
 }
+
+/* Prettier usage
+Many points in prettier are in conflics with current standardJS version:
+  - single quotes in JSX
+  - space before parenthesis in function definition
+
+Since prettier is considered as a much better formatter
+(different from a linter), we keep in this eslint config only
+standardJS config/plugin. It's why prettier specific errors
+won't be displayed using linting here (for rules about code wrapping
+for example).
+
+But prettier can be used as a formater only while keeping standardJS
+compatibilty by using both commands: */
+//   prettier --write '{src,test}/**/*.{js,jsx}' && eslint --fix '{src,test}/**/*.{js,jsx}'
