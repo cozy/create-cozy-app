a1855cfd1381f5b66e21d372c040e72c827ec235
Patrick Browne <pt.browne@gmail.com>
2017-10-20 19:21:18 +0200

    feat: add webpack-dev-server hot feature

--- packages/cozy-scripts/scripts/standalone.js
+++ packages/cozy-scripts/scripts/standalone.js
@@ -1,24 +1,31 @@
 'use strict'
 
 const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
+const webpackDevServer = require('webpack-dev-server')
 const colorize = require('../utils/_colorize.js')
 const appConfig = require('./config')
 
-const compiler = webpack(Object.assign({}, appConfig, {
+const config = Object.assign({}, appConfig, {
   output: {
     filename: '[name][hash].bundle.js'
   }
-}))
-
-const server = new WebpackDevServer(compiler, {
-  stats: { colors: true },
-  inline: true,
-  hot: true
 })
 
 const port = process.env.PORT || '8080'
 const host = process.env.HOST || 'localhost'
+
+const options = {
+  stats: { colors: true },
+  inline: true,
+  hot: true,
+  host,
+  port
+}
+
+webpackDevServer.addDevServerEntrypoints(config, options);
+
+const compiler = webpack(config)
+const server = new webpackDevServer(compiler, options)
 
 server.listen(port, host, err => {
   console.log()
