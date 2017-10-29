b052c28ccc40080969057f0c16d7d3f49e58b39f
CPatchane <code@patchane.com>
2017-10-29 21:10:32 +0100

    fix: better and simpler sidebar items + clean up some icons :nail_care:

--- packages/cozy-scripts/template/app/src/components/Nav.jsx
+++ packages/cozy-scripts/template/app/src/components/Nav.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import { translate } from 'cozy-ui/react/I18n'
+import { translate, Icon } from 'cozy-ui/react'
 import { NavLink } from 'react-router-dom'
+import NavIcon from '../assets/icons/icon-pin.svg'
 
 export const Nav = ({ t }) => (
   <nav>
     <ul className='c-nav'>
       <li className='c-nav-item'>
-        <NavLink to='/viewhello1' className='hello-cat-hello1 c-nav-link' activeClassName='active'>
+        <NavLink to='/viewhello1' className='c-nav-link' activeClassName='active'>
+          <Icon icon={NavIcon} />
           { t('Nav.hello_nav_1') }
         </NavLink>
       </li>
       <li className='c-nav-item'>
-        <NavLink to='/viewhello2' className='hello-cat-hello2 c-nav-link' activeClassName='active'>
+        <NavLink to='/viewhello2' className='c-nav-link' activeClassName='active'>
+          <Icon icon={NavIcon} />
           { t('Nav.hello_nav_2') }
         </NavLink>
       </li>
       <li className='c-nav-item'>
-        <NavLink to='/viewhello3' className='hello-cat-hello3 c-nav-link' activeClassName='active'>
+        <NavLink to='/viewhello3' className='c-nav-link' activeClassName='active'>
+          <Icon icon={NavIcon} />
           { t('Nav.hello_nav_3') }
         </NavLink>
       </li>
