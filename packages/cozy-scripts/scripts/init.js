de4613e931c1e61785d70206d1ee4b7820a7d30e
CPatchane <code@patchane.com>
2018-01-15 09:46:09 +0100

    fix: typo :pencil2:

--- packages/cozy-scripts/scripts/init.js
+++ packages/cozy-scripts/scripts/init.js
@@ -28,7 +28,7 @@ module.exports = function (appPath, appName, cliOptions, gracefulRootExit, overr
       conform: function (value) {
         return validateProjectName(value).validForNewPackages
       },
-      message: 'Must be maily lowercase letters, digits or dashes (see NPM name requirements)',
+      message: 'Must be mainly lowercase letters, digits or dashes (see NPM name requirements)',
       required: false,
       default: appName
     },
