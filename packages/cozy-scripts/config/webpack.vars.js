3e77a670036d9788955724dff2f049c361378c99
CPatchane <code@patchane.com>
2018-02-17 12:26:17 +0100

    fix: console log the used environment and target when compiling :nail_care:

--- packages/cozy-scripts/config/webpack.vars.js
+++ packages/cozy-scripts/config/webpack.vars.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const colorize = require('../utils/_colorize')
 
 // default NODE_ENV to browser development
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'browser:development'
@@ -8,6 +9,8 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'browser:development'
 const target = process.env.NODE_ENV.match(/^(\w+):/)[1]
 const environment = process.env.NODE_ENV.match(/^\w+:(\w+)$/)[1]
 const production = environment === 'production'
+
+console.log(colorize.cyan(`Compiling for ${colorize.orange.bold(environment)} environment and ${colorize.orange.bold(target)} target...`))
 
 const isDebugMode = process.env.COZY_SCRIPTS_DEBUG === 'true'
 
