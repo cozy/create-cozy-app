32b6483306338f3f2a13287e6a1df5d83481fc75
CPatchane <code@patchane.com>
2018-03-19 09:25:10 +0100

    fix: typo in a path + yarn.lock changes :pencil2:

--- packages/cozy-scripts/bin/cozy-scripts.js
+++ packages/cozy-scripts/bin/cozy-scripts.js
@@ -5,7 +5,7 @@
 const commander = require('commander')
 const pkg = require('../package.json')
 const colorize = require('../utils/_colorize.js')
-const getWebpackConfigs = require('./config')
+const getWebpackConfigs = require('../scripts/config')
 
 let actionName
 const program = new commander.Command(pkg.name)
