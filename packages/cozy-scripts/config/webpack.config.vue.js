d14e56e71d342ec3697ac524b2660003bdd2a77a
CPatchane <code@patchane.com>
2018-05-09 00:15:06 +0200

    :arrow_down: temporary downgrade breaking vue-loader from v15 to v14

--- packages/cozy-scripts/config/webpack.config.vue.js
+++ packages/cozy-scripts/config/webpack.config.vue.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-
 process.env.__ENTRY_EXT__ = '.js'
 
 module.exports = {
@@ -14,8 +12,5 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'vue-loader'
     }]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
+  }
 }
