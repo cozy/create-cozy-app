b5191552454ad7a78f743e83ef76eae3d7d35e58
CPatchane <code@patchane.com>
2018-05-19 18:23:02 +0200

    :wrench: chore: better babel configurations

--- packages/cozy-scripts/config/webpack.config.preact.js
+++ packages/cozy-scripts/config/webpack.config.preact.js
@@ -19,7 +19,8 @@ module.exports = {
       exclude: /node_modules\/(?!(cozy-ui))/,
       loader: 'babel-loader',
       options: {
-        cacheDirectory: 'node_modules/.cache/babel-loader'
+        cacheDirectory: 'node_modules/.cache/babel-loader/react',
+        presets: ['cozy-app']
       }
     }]
   }
