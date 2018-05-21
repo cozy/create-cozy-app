1616e0374a734fb686f81ec1689f20e6af720788
CPatchane <code@patchane.com>
2018-05-21 18:26:47 +0200

    :rotating_light: fix: lint issues in vue template

--- packages/cozy-scripts/template-vue/app/src/components/Icon.vue
+++ packages/cozy-scripts/template-vue/app/src/components/Icon.vue
@@ -10,54 +10,54 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      icon: {
-        type: [String, Object],
-        default: null
-      },
-      iconWidth: {
-        type: String,
-        default: '1em'
-      },
-      color: {
-        type: String,
-        default: ''
-      },
-      iconHeight: {
-        type: String,
-        default: '1em'
-      },
-      iconClass: {
-        type: String,
-        default: ''
-      },
-      iconStyle: {
-        type: Object,
-        default: null
-      },
+export default {
+  props: {
+    icon: {
+      type: [String, Object],
+      default: null
     },
-    computed: {
-      computedStyle () {
-        if (this.color) {
-          return ({
-              fill: this.color
-          })
+    iconWidth: {
+      type: String,
+      default: '1em'
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    iconHeight: {
+      type: String,
+      default: '1em'
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    },
+    iconStyle: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    computedStyle() {
+      if (this.color) {
+        return {
+          fill: this.color
         }
-      },
-      anchor () {
-        let anchor
-        if (this.icon.id) {
-          anchor = `#${this.icon.id}`
-        } else if (this.icon[0] === '#') {
-          anchor = this.icon
-        }
-
-        if (!anchor) {
-          throw new Error(`Icon not found ${this.icon}.`)
-        }
-        return anchor
       }
+    },
+    anchor() {
+      let anchor
+      if (this.icon.id) {
+        anchor = `#${this.icon.id}`
+      } else if (this.icon[0] === '#') {
+        anchor = this.icon
+      }
+
+      if (!anchor) {
+        throw new Error(`Icon not found ${this.icon}.`)
+      }
+      return anchor
     }
   }
+}
 </script>
