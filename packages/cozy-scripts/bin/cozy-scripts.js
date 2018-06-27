976d7910649dba172f8e6a0d25244b404a9205e7
CPatchane <code@patchane.com>
2018-06-27 11:49:21 +0200

    :nail_care: add --hot option to cozy-scripts

--- packages/cozy-scripts/bin/cozy-scripts.js
+++ packages/cozy-scripts/bin/cozy-scripts.js
@@ -18,11 +18,15 @@ const program = new commander.Command(pkg.name)
   .option('--show-config', 'just print app final webpack config')
   .option('--debug', 'print more outputs for debugging')
   .option('--development', 'specify development build mode')
+  .option('--hot', 'enable hot module reload (only for development)')
   .option('--production', 'specify production build mode')
   .option('--browser', 'specify browser build target')
   .option('--mobile', 'specify mobile build target')
   .option('--vue', 'to use scripts in a VueJS specific way (default (p)React)')
-  .option('--analyzer', 'open an analyzer with an interactive treemap visualization of the contents of all builds')
+  .option(
+    '--analyzer',
+    'open an analyzer with an interactive treemap visualization of the contents of all builds'
+  )
   .parse(process.argv)
 
 // build mode and target computing (overwritten by NODE_ENV)
@@ -33,9 +37,7 @@ const options = {
     (actionName === 'build' && 'production') ||
     'development',
   target:
-    (program.browser && 'browser') ||
-    (program.mobile && 'mobile') ||
-    'browser',
+    (program.browser && 'browser') || (program.mobile && 'mobile') || 'browser',
   useVue: program.vue,
   debugMode: program.debug,
   bundleAnalyzer: program.analyzer,
@@ -43,10 +45,12 @@ const options = {
   cliArgs: process.argv.slice(3)
 }
 
+if (program.hot) {
+  process.env.HOT_RELOAD = 'true'
+}
+
 if (program.showConfig) {
-  console.log(JSON.stringify(
-    getWebpackConfigs(options), null, 2)
-  )
+  console.log(JSON.stringify(getWebpackConfigs(options), null, 2))
   process.exit(0)
 }
 
