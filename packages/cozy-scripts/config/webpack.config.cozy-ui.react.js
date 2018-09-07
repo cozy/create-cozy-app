5fe182627fbec5ccc8ddc498f4f8a82517f0d320
CPatchane <code@patchane.com>
2018-09-07 14:34:54 +0200

    fix: :fire: replace ETWP by mini-css-extract-plugin for css build

--- packages/cozy-scripts/config/webpack.config.cozy-ui.react.js
+++ packages/cozy-scripts/config/webpack.config.cozy-ui.react.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { extractor } = require('./webpack.vars')
+const { getCSSLoader } = require('./webpack.vars')
 
 module.exports = {
   module: {
@@ -8,30 +8,28 @@ module.exports = {
       {
         test: /\.styl$/,
         include: /(cozy-ui\/react)/,
-        loader: extractor.extract({
-          fallback: require.resolve('style-loader'),
-          use: [
-            {
-              loader: require.resolve('css-loader'),
-              options: {
-                sourceMap: true,
-                importLoaders: 1,
-                modules: true,
-                localIdentName: '[local]--[hash:base64:5]'
+        use: [
+          getCSSLoader(),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              sourceMap: true,
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[local]--[hash:base64:5]'
+            }
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              sourceMap: true,
+              plugins: function () {
+                return [ require('autoprefixer')({ browsers: ['last 2 versions'] }) ]
               }
-            },
-            {
-              loader: require.resolve('postcss-loader'),
-              options: {
-                sourceMap: true,
-                plugins: function () {
-                  return [ require('autoprefixer')({ browsers: ['last 2 versions'] }) ]
-                }
-              }
-            },
-            require.resolve('stylus-loader')
-          ]
-        })
+            }
+          },
+          require.resolve('stylus-loader')
+        ]
       }
     ]
   }
--- packages/cozy-scripts/config/webpack.environment.prod.js
+++ packages/cozy-scripts/config/webpack.environment.prod.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'), // to compile on production mode (redux)
       __DEVELOPMENT__: false,
       __DEVTOOLS__: false,
       __STACK_ASSETS__: target !== 'mobile'
