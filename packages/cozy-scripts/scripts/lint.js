55ca7193c69aa38591145f457103d8af7f17dbc2
Cyrille Perois <cperois@outlook.com>
2018-10-16 16:52:58 +0200

    feat(lint): Add log

--- packages/cozy-scripts/scripts/lint.js
+++ packages/cozy-scripts/scripts/lint.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const spawn = require('cross-spawn')
+const colorize = require('../utils/_colorize')
 
 module.exports = () => {
   const argv = process.argv.slice(3)
 
+  console.info(colorize.orange(`eslint ${argv.join(' ')}`))
   spawn.sync('eslint', argv, { stdio: 'inherit' })
 }
