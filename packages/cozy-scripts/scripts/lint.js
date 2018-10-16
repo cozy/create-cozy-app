fa84798e4d0b199a931aef5d75c317ef92a4a0b8
Cyrille Perois <cperois@outlook.com>
2018-10-16 16:49:32 +0200

    feat: No hardcoded globs

--- packages/cozy-scripts/scripts/lint.js
+++ packages/cozy-scripts/scripts/lint.js
@@ -2,16 +2,10 @@
 
 const { exec } = require('child_process')
 
-const globs = {
-  react: '{src,test}/**/*.{js,jsx}',
-  vue: '{src,test}/**/*.{js,vue}'
-}
-
-module.exports = buildOptions => {
+module.exports = () => {
   const argv = process.argv.slice(3)
-  const glob = buildOptions.useVue ? globs.vue : globs.react
 
-  exec(`eslint '${glob}' ${argv.join(' ')}`, (err, stdout) => {
+  exec(`eslint ${argv.join(' ')}`, (err, stdout) => {
     console.log(stdout)
   })
 }
