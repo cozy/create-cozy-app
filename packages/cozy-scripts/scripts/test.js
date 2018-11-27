5558cbe09d6fe1f430a43fe74f0971afc4bf0762
CPatchane <code@patchane.com>
2018-11-27 19:40:00 +0100

    fix: :bug: Don't automatically pass config to jest

--- packages/cozy-scripts/scripts/test.js
+++ packages/cozy-scripts/scripts/test.js
@@ -1,15 +1,10 @@
 'use strict'
 
 const jestAPI = require('jest')
-const fs = require('fs-extra')
-const paths = require('../utils/paths')
 
 function runJest(options) {
   const { cliArgs } = options
 
-  if (fs.existsSync(paths.jestConfig)) {
-    cliArgs.push('--config', paths.jestConfig)
-  }
   jestAPI.run(cliArgs)
 }
 
