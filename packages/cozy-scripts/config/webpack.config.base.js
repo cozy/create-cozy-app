675648f5640eec14f14a4ce115e2c6dc37560b4e
CPatchane <code@patchane.com>
2019-01-23 00:50:16 +0100

    fix: :bug: Use relative node_modules path
    Can be considered as a BREAKING CHANGE since
    it could break some modules resolving

--- packages/cozy-scripts/config/webpack.config.base.js
+++ packages/cozy-scripts/config/webpack.config.base.js
@@ -16,7 +16,7 @@ const production = environment === 'production'
 
 module.exports = {
   resolve: {
-    modules: [paths.appSrc(), paths.appNodeModules()],
+    modules: [paths.appSrc(), 'node_modules'],
     extensions: ['.js', '.json', '.css'],
     // linked package will still be see as a node_modules package
     symlinks: false
