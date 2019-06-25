b63292b6372da1ee1ca195cb621933d29b57b958
CPatchane <code@patchane.com>
2019-06-25 12:53:06 +0200

    fix: :boom: Use identity-obj-proxy with a regex in jest config

--- packages/cozy-scripts/template-vue/app/jest.config.js
+++ packages/cozy-scripts/template-vue/app/jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(png|gif|jpe?g|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
     // identity-obj-proxy module is installed by cozy-scripts
-    styles: 'identity-obj-proxy'
+    '\\.(css|styl)$': 'identity-obj-proxy'
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   transformIgnorePatterns: ['node_modules/(?!cozy-ui)'],
