2dccdac880574d8263cdab74ae48a200fbd3d480
Yoan VALLET <ext.sopra.yvallet@grandlyon.com>
2020-08-28 12:15:12 +0200

    replace "\/" by "(\/|\\)" to be handle by windows

--- packages/cozy-scripts/config/webpack.config.react.js
+++ packages/cozy-scripts/config/webpack.config.react.js
@@ -13,7 +13,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx$/,
-        exclude: /node_modules\/(?!(cozy-ui))/,
+        exclude: /node_modules(\/|\\)(?!(cozy-ui))/,
         loader: require.resolve('babel-loader'),
         options: {
           cacheDirectory: 'node_modules/.cache/babel-loader/react',
