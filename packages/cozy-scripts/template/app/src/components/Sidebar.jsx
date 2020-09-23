b77a39c3c9f70a6932477c1aed04ac2222d653c2
Patrick Browne <pt.browne@gmail.com>
2020-09-23 12:57:31 +0200

    feat: Refresh sidebar

--- packages/cozy-scripts/template/app/src/components/Sidebar.jsx
+++ packages/cozy-scripts/template/app/src/components/Sidebar.jsx
@@ -1,46 +1,42 @@
 import React from 'react'
-import Icon from 'cozy-ui/react/Icon'
 import { translate } from 'cozy-ui/react/I18n'
-import { NavLink } from 'react-router-dom'
-import NavIcon from '../assets/icons/icon-bullet-point.svg'
+import { NavLink as RRNavLink } from 'react-router-dom'
+
+import UISidebar from 'cozy-ui/transpiled/react/Sidebar'
+import Nav, {
+  NavItem,
+  NavIcon,
+  NavText,
+  genNavLink
+} from 'cozy-ui/transpiled/react/Nav'
+
+import navIcon from '../assets/icons/icon-bullet-point.svg'
+
+const NavLink = genNavLink(RRNavLink)
 
 export const Sidebar = ({ t }) => (
-  <aside className="o-sidebar">
-    <nav>
-      <ul className="c-nav">
-        <li className="c-nav-item">
-          <NavLink
-            to="/todos"
-            className="c-nav-link"
-            activeClassName="is-active"
-          >
-            <Icon className="c-nav-icon" icon={NavIcon} />
-            {t('Nav.todos')}
-          </NavLink>
-        </li>
-        <li className="c-nav-item">
-          <NavLink
-            to="/viewhello1"
-            className="c-nav-link"
-            activeClassName="is-active"
-          >
-            <Icon className="c-nav-icon" icon={NavIcon} />
-            {t('Nav.hello_nav_2')}
-          </NavLink>
-        </li>
-        <li className="c-nav-item">
-          <NavLink
-            to="/viewhello2"
-            className="c-nav-link"
-            activeClassName="is-active"
-          >
-            <Icon className="c-nav-icon" icon={NavIcon} />
-            {t('Nav.hello_nav_3')}
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </aside>
+  <UISidebar>
+    <Nav>
+      <NavItem>
+        <NavLink to="/todos">
+          <NavIcon icon={navIcon} />
+          <NavText>{t('Nav.todos')}</NavText>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink to="/viewhello1">
+          <NavIcon icon={navIcon} />
+          <NavText>{t('Nav.hello_nav_2')}</NavText>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink to="/viewhello2">
+          <NavIcon icon={navIcon} />
+          <NavText>{t('Nav.hello_nav_3')}</NavText>
+        </NavLink>
+      </NavItem>
+    </Nav>
+  </UISidebar>
 )
 
 // translate() provide t() to use translations (ex: locales/en.json)
