43dc4d91bb0e557aef8958559b1a656f6a5d0c0e
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-03-18 15:40:27 +0100

    feat(service-workers): Add webpack config to handle service worker

--- packages/cozy-scripts/config/webpack.bundle.default.js
+++ packages/cozy-scripts/config/webpack.bundle.default.js
@@ -20,6 +20,7 @@ const configs = [
   require('./webpack.config.duplicates'),
   addAnalyzer ? require('./webpack.config.analyzer') : null,
   require('./webpack.config.services'),
+  require('./webpack.config.workers'),
   require(`./webpack.target.${target}`)
 ]
 
--- packages/cozy-scripts/utils/paths.js
+++ packages/cozy-scripts/utils/paths.js
@@ -54,6 +54,7 @@ module.exports = {
   appPath: () => resolveApp(),
   appBuild: () => resolveAppBuild(),
   appServicesBuild: () => resolveAppBuild('services'),
+  appServicesWorkersBuild: () => resolveAppBuild('serviceWorker'),
   appBuildAssetsJson: () => resolveAppBuild('assets/json'),
   appManifest: () => resolveAppManifest(),
   appREADME: () => resolveApp('README.md'),
@@ -75,6 +76,8 @@ module.exports = {
   appPublicIndex: () => resolveSrcWithExtension('targets/public/index'),
   // for services
   appServicesFolder: () => resolveAppSrc('targets/services'),
+  // for services-workers
+  appServicesWorkersFolder: () => resolveAppSrc('serviceWorker'),
   // for mobile
   appMobileHtmlTemplate: () => resolveAppSrc('targets/mobile/index.ejs'),
   appMobileIndex: () => resolveSrcWithExtension('targets/mobile/index'),
