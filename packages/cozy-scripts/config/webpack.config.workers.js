faa8d1a44ee569eb78575d364ae1510d10bb10af
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-03-18 15:40:27 +0100

    feat(service-workers): Add webpack config to handle service worker

---
+++ packages/cozy-scripts/config/webpack.config.workers.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs-extra')
+const webpack = require('webpack')
+const paths = require('../utils/paths')
+const { getFilename } = require('./webpack.vars')
+
+const servicesWorkersFolder = paths.appServicesWorkersFolder()
+const servicesWorkersPaths = fs.existsSync(servicesWorkersFolder)
+  ? fs.readdirSync(servicesWorkersFolder)
+  : []
+
+const servicesWorkersEntries = {}
+servicesWorkersPaths.forEach(file => {
+  if (!file.match(/^[^.]*.js$/)) return
+  var filename = file.match(/^([^.]*).js$/)[1]
+  servicesWorkersEntries[filename] = path.resolve(
+    path.join(servicesWorkersFolder, file)
+  )
+})
+
+const config = {
+  __mergeStrategy: {
+    smart: false,
+    strategy: {
+      plugins: 'replace',
+      output: 'replace',
+      entry: 'replace',
+      optimization: 'replace',
+      module: 'replace',
+      externals: 'replace'
+    }
+  },
+  entry: servicesWorkersEntries,
+  output: {
+    path: paths.appServicesWorkersBuild(),
+    filename: `${getFilename(false)}.js`
+  },
+  target: 'node',
+  optimization: {}, // reset optimization property
+  devtool: false,
+  externals: [], // reset externals property
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: 'node_modules/.cache/babel-loader/node',
+          babelrc: false,
+          presets: [['cozy-app', { node: true, react: false }]]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __TARGET__: JSON.stringify('servicesWorkers')
+    })
+  ]
+}
+
+// only for browser target (servicesWorkers are usable only on cozy-stack)
+module.exports = { multiple: { servicesWorkers: config } }
