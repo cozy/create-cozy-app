f50751b39e6647ae5b385bb0bee89a5ace2901dc
Merkur39 <merkkur39@gmail.com>
2022-12-12 09:19:35 +0100

    chore(cca-template): Add default helpers for tests

--- packages/cozy-scripts/template/app/test/jestLib/setup.js
+++ packages/cozy-scripts/template/app/test/jestLib/setup.js
@@ -9,5 +9,14 @@ global.requestAnimationFrame = cb => {
 /* Fix error: "Material-UI: The color provided to augmentColor(color) is invalid.
 The color object needs to have a `main` property or a `500` property."*/
 jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
-  getCssVariableValue: () => '#fff'
+  getCssVariableValue: () => '#fff',
+  getInvertedCssVariableValue: () => '#000'
 }))
+
+window.cozy = {
+  bar: {
+    BarLeft: () => null,
+    BarCenter: () => null,
+    BarRight: () => null
+  }
+}
